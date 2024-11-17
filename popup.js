c017ca3c62a3d5b0721bab10a0bc735d119f4f5a
H0DLR <160393311+h0dlr@users.noreply.github.com>
2024-11-17 13:09:28 +0000

    Update popup.js

--- popup.js
+++ popup.js
@@ -4,10 +4,10 @@ const walletIframe = document.getElementById('walletIframe');
 // Load the appropriate URL based on the app version setting.
 chrome.storage.sync.get(['appVersion'], function (result) {
   const appVersion = result.appVersion || 'production'; // Default to production if setting not found.
-  let walletUrl = 'https://wallet.nutstash.app';
+  let walletUrl = 'https://coinos.io/';
 
   if (appVersion === 'beta') {
-    walletUrl = 'https://alpha.nutstash.app';
+    walletUrl = 'https://coinos.io/';
   }
 
   walletIframe.src = walletUrl;
